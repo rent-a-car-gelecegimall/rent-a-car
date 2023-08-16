7999c0bb4fe1712f43775dc7149dfe80e357edb2
Hasan Burak Songur <123594944+hburaks@users.noreply.github.com>
2023-08-16 15:19:12 +0300

    create backend configs

---
+++ rent-a-car-backend/src/main/java/com/gelecegimall/rentacarbackend/RentACarBackendApplication.java
@@ -0,0 +1,13 @@
+package com.gelecegimall.rentacarbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentACarBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RentACarBackendApplication.class, args);
+    }
+
+}
---
+++ rent-a-car-backend/src/test/java/com/gelecegimall/rentacarbackend/RentACarBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gelecegimall.rentacarbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentACarBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
